750b75cb93c3540c12e493f031e0f36a4d57707f
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-07-30 17:41:06 +0400

    fix default attrs
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,25 @@
 default['libvirt']['hooks'] = []
-default['libvirt']['service'] = 'libvirtd'
+default['libvirt']['packages'] = value_for_platform(
+  [ 'centos', 'redhat', 'suse', 'fedora' ] => {
+    'default' => 'libvirt'
+  },
+  [ 'exherbo', 'archlinux' ] => {
+    'default' => 'libvirt'
+  },
+  [ 'ubuntu', 'debian'] => {
+    'default' => [ 'libvirt-bin' ]
+  }
+)
+default['libvirt']['service'] = value_for_platform(
+  [ 'centos', 'redhat', 'suse', 'fedora' ] => {
+    'default' => 'libvirt'
+  },
+  [ 'exherbo', 'archlinux' ] => {
+    'default' => 'libvirtd'
+  },
+  [ 'ubuntu', 'debian'] => {
+    'default' => [ 'libvirt-bin' ]
+  }
+)
+
 default['libvirt']['conf.d'] = '/etc/libvirt'
