72aba7d50fe2b7d9dea5a1b926b136564fd7ea57
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-08-07 17:39:27 +0400

    fix code duplication
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,41 +4,6 @@ service node['libvirt']['libvirt_service'] do
   supports [ :start, :stop, :status, :reload ]
 end
 
-unless node['libvirt']['hooks'].nil?
-
-  node['libvirt']['hooks'].each do |name, options|
-    next unless options['type'].nil?
-
-    hook_path = ''
-    hook_dir = ''
-
-    case options['type']
-    when 'daemon', 'qemu', 'lxc', 'network'
-      hook_dir = ::File.join(node['libvirt']['conf.d'], 'hooks', options['type'])
-      hook_path = ::File.join(hook_dir, name)
-    else
-      Chef::Log.error('unknown libvirt hook type: ' + options['type'])
-    end
-
-    directory hook_dir do
-      owner node['libvirt']['user']
-      group node['libvirt']['group']
-      mode 00750
-      recursive true
-      action :create
-    end
-
-    file hook_path do
-      content options['content']
-      mode 00750
-      owner node['libvirt']['user']
-      group node['libvirt']['group']
-      action :create
-      notifies :reload, "service[#{node['libvirt']['libvirt_service']}]", :delayed
-    end
-  end
-end
-
 template '/etc/libvirt/libvirtd.conf' do
   source 'libvirtd.conf.erb'
   owner node['libvirt']['user']
@@ -79,7 +44,7 @@ end
 
 unless node['libvirt']['hooks'].nil?
   node['libvirt']['hooks'].each do |hook|
-    libvirt_hook hook['type'] do
+    libvirt_hook hook['name'] do
       %w{name source}.each do |attr|
         send(attr, pool[attr])  if pool[attr]
       end
