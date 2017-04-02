b63cb1411bcd15a17c19821f6ca4af6485a437ad
Vasiliy Tolstov <v.tolstov@selfip.ru>
2017-04-02 15:29:05 +0300

    update metadata
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,11 @@ description 'Installs/configures libvirt'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/vtolstov/cb-libvirt' if respond_to?(:source_url)
 issues_url       'https://github.com/vtolstov/cb-libvirt/issues' if respond_to?(:issues_url)
-version '0.0.2'
+version '0.0.3'
 
 recipe 'libvirt', 'Installs libvirt package and sets up configuration'
 
-%w(sheepdog openvswitch systemd blockdev filesystem).each do |dep|
+%w(chef_sheepdog chef_openvswitch chef_systemd chef_blockdev chef_filesystem).each do |dep|
   depends dep
 end
 
